7b844b7e97549e00f083f032e89be96192a7d299
car46jr <91435074+car46jr@users.noreply.github.com>
2022-10-15 09:33:45 -0600

    Carlos

--- src/java/Clases/AlumnoController.java
+++ src/java/Clases/AlumnoController.java
@@ -4,10 +4,63 @@
  */
 package Clases;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
- * @author brand
+ * @carjr
  */
 public class AlumnoController {
+      Alumno[] tablaALumno;
+    int indiceArray;
+    /**private ConexionBaseDeDatos conectorBD;*/
+    private Connection conexion;
+    private PreparedStatement statement = null;
+    private ResultSet result = null;
     
+    public AlumnoController(){
+        this.tablaALumno = new Alumno[100];
+        this.indiceArray=0;
+    }
+    
+     public void guardarAlumno(Alumno alumno){
+        this.tablaALumno[this.indiceArray]=alumno;  
+        this.indiceArray=this.indiceArray+1;
+        // procedimiento para almacenar en la Base de Datos
+    }
+    
+    public Alumno[] getAlumnos(){
+        return this.tablaALumno;
+    }
+    /*
+    public void abrirConexion(){
+        conectorBD= new ConexionBaseDeDatos();
+        conexion=conectorBD.conectar();
+    }
+    
+    public boolean guardarAlumno2(Alumno alumno){        
+        String sql = "INSERT INTO universidad.alumno(numero_carne, nombre, correo, genero_idgenero) ";
+             sql += " VALUES( ?,?,?,?)"; 
+        try{
+            abrirConexion();
+            statement = conexion.prepareStatement(sql); 
+            statement.setInt(1, alumno.getCodigo());
+            statement.setString(2, alumno.getNombre());
+            statement.setString(3, alumno.getCorreo());
+             int resultado = statement.executeUpdate(); 
+                if(resultado > 0){
+                    return true;
+                }else{
+                    return false;
+                }
+        }catch(SQLException e){
+            String error = e.getMessage();  
+            return false;
+        }    
+    }
+    
+    */
 }
